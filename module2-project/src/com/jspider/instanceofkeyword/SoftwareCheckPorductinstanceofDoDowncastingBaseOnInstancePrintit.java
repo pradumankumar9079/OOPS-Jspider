7b14203d8e740da5b8c602804f7156a46af5e0d6
pradumankumar9079 <pradumankumar9079@gmail.com>
2024-10-08 20:33:34 +0530

    day 6

--- module2-project/src/com/jspider/instanceofkeyword/SoftwareCheckPorductinstanceofDoDowncastingBaseOnInstancePrintit.java
+++ module2-project/src/com/jspider/instanceofkeyword/SoftwareCheckPorductinstanceofDoDowncastingBaseOnInstancePrintit.java
@@ -2,28 +2,31 @@ package com.jspider.instanceofkeyword;
 
 class SoftwareEngineer {
 	void meeting() {
-
+		System.out.println("Executing meeting()...");
 	}
 }
 
 class SoftwareDeveloper extends SoftwareEngineer {
 	void coding() {
+		System.out.println("Executing coding()...");
+	}
+}
 
-	}}
-
-wclass TestEngineer extends SoftwareEngineer{
+class TestEngineer extends SoftwareEngineer {
 
 	void testing() {
-
+		System.out.println("Executing testing()...");
 	}
 }
 
 class Manager {
 	static void assignTask(SoftwareEngineer s) {
 		if (s instanceof SoftwareDeveloper) {
-			SoftwareDeveloper dev = (SoftwareDeveloper) s;// Downcasting now
+			// Downcasting now
+			SoftwareDeveloper dev = (SoftwareDeveloper) s;
 			// access testing means properties of SoftwareDeveloper
 			dev.coding();
+			dev.meeting();
 		} else if (s instanceof TestEngineer) {
 			TestEngineer test = (TestEngineer) s;// Downcasting now
 			// access testing means properties of SoftwareDeveloper
@@ -33,5 +36,8 @@ class Manager {
 }
 
 public class SoftwareCheckPorductinstanceofDoDowncastingBaseOnInstancePrintit {
-
+	public static void main(String[] args) {
+		Manager.assignTask(new SoftwareDeveloper());
+		Manager.assignTask(new TestEngineer());
+	}
 }
