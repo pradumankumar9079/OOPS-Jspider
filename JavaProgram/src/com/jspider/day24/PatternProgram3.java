d6b9ad57440c65f74ee76a0b9d2d9e9a821cec9c
pradumankumar9079 <pradumankumar9079@gmail.com>
2024-10-10 18:58:56 +0530

    day 11

---
+++ JavaProgram/src/com/jspider/day24/PatternProgram3.java
@@ -0,0 +1,16 @@
+package com.jspider.day24;
+
+public class PatternProgram3 {
+	public static void main(String[] args) {
+		int n = 5;
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n - i; j++) {
+				System.out.print(" ");
+			}
+			for (int k = i; k >= 1; k++) {
+				System.out.print(k);
+			}
+			System.out.println();
+		}
+	}
+}
