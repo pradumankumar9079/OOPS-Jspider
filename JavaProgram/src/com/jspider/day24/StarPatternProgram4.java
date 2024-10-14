c65289c0abaa57dd347fb2197e81f8c1986be558
pradumankumar9079 <pradumankumar9079@gmail.com>
2024-10-14 23:27:40 +0530

    day1

--- JavaProgram/src/com/jspider/day24/StarPatternProgram4.java
+++ JavaProgram/src/com/jspider/day24/StarPatternProgram4.java
@@ -1,5 +1,26 @@
 package com.jspider.day24;
 
+/*
+*********
+ *******
+  *****
+   ***
+    *
+*/
 public class StarPatternProgram4 {
-
+	public static void main(String[] args) {
+		int n = 5;
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= i - 1; j++) {
+				System.out.print(" ");
+			}
+			for (int k = 1; k <= n - i; k++) {
+				System.out.print("*");
+			}
+			for (int k = 1; k <= n - i + 1; k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
 }
