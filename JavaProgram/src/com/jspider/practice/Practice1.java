d9b76713da2a530a3f2484fb974c1c6d9b756560
pradumankumar9079 <pradumankumar9079@gmail.com>
2024-11-22 14:58:08 +0530

    practice

--- JavaProgram/src/com/jspider/practice/Practice1.java
+++ JavaProgram/src/com/jspider/practice/Practice1.java
@@ -3,7 +3,7 @@ package com.jspider.practice;
 // Reverse Each Word in a Sentence
 public class Practice1 {
 	public static void main(String[] args) {
-		String s = "ram is good";
+		String s = "rgham i69s gopk0d";
 		char[] ch = s.toCharArray();
 		int firstIndex = 0;
 		for (int i = 0; i <= s.length() - 1; i++) {
@@ -11,9 +11,15 @@ public class Practice1 {
 				firstIndex = i;
 			}
 			if (i == ch.length - 1 && ch[i] != ' ' || ch[i] != ' ' && ch[i + 1] == ' ') {
-				char temp = ch[firstIndex];
-				ch[firstIndex] = ch[i];
-				ch[i] = temp;
+				int fi = firstIndex;
+				int li = i;
+				while (fi <= li) {
+					char temp = ch[fi];
+					ch[fi] = ch[li];
+					ch[li] = temp;
+					fi++;
+					li--;
+				}
 			}
 		}
 		System.out.println(ch);
