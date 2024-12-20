78f3ac82a7bbc0cf1ad2cd3667faa7de6ab5402c
pradumankumar9079 <pradumankumar9079@gmail.com>
2024-12-20 20:24:54 +0530

    practice

--- module3/src/practice/MainClass1.java
+++ module3/src/practice/MainClass1.java
@@ -1,19 +1,33 @@
 package practice;
 
-class A {
-	A() {
+class A extends Thread {
+	@Override
 
+	public void run() {
+		for (int i = 0; i <= 10; i++) {
+//			System.out.println(i);
+			Thread th = Thread.currentThread();
+			System.out.println("ID: " + th.getId() + "Name: " + th.getName() + "Priority: " + th.getPriority());
+		}
 	}
-}
 
-class B extends A {
-	void A() {
-
+	public void test() {
+		for (int i = 11; i <= 20; i++) {
+			Thread th = Thread.currentThread();
+			System.out.println("ID: " + th.getId() + "Name: " + th.getName() + "Priority: " + th.getPriority());
+//			System.out.println();
+		}
 	}
 }
 
 public class MainClass1 {
 	public static void main(String[] args) {
+		A ref = new A();
+		Thread th = new Thread(ref);
+		th.start();
+		th.setName("Raja ");
+		th.setPriority(10);
 
+		ref.test();
 	}
 }
